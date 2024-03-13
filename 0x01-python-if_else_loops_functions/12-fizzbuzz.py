9f857f29b3afb06ee2f6f7be8788b60b2553740a
“zawayaalex” <zawayaalex@gmail.com>
2024-03-13 04:05:09 -0700

    where

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range (0, 100):
-        if (i % 15):
+        if (i % 15 == 0):
             print("FizzBuzz ")
         else:
-            if (i % 3):
+            if (i % 3 == 0):
                 print("Fizz ")
         else:
-            if (i % 5):
+            if (i % 5 == 0):
                 print("Buzz ")
         else:
-            print(i," ")
+            print(i, " ")
