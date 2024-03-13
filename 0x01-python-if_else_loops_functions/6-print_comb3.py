0932b228277d213093a6a643d5200a5437d75190
“zawayaalex” <zawayaalex@gmail.com>
2024-03-12 18:07:08 -0700

    compo

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for n in range(0, 10):
+    for m in range(n + 1, 10):
+        if n == 8 and m == 9:
+            print("{}{}".format(n, m))
+        else:
+            print("{}{}".format(n, m), end=", ")
+            
