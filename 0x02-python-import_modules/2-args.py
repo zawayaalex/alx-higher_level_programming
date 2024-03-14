0a99e97042269301303bb6588440d25221ab9cd7
“zawayaalex” <zawayaalex@gmail.com>
2024-03-14 14:00:09 -0700

    modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+        import sys
+            i = len(sys.argv) - 1
+                if i == 0:
+                            print("0 arguments.")
+                elif i == 1:
+                            print("1 argument:")
+                else:
+                            print("{} arguments".format(i))
+                                for n in range (i):
+                                            print("{}: {}".format(n + 1, sys.argv[n + 1]))
