875fb3f886b8c80a68672247f5da9f37ba560e89
“zawayaalex” <zawayaalex@gmail.com>
2024-03-14 14:25:52 -0700

    modules

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    n = 0
+    count = len(sys.argv) - 1
+    for i in range(count):
+        n = n + int (sys.argv[i + 1])
+    print("{}".format(n))
