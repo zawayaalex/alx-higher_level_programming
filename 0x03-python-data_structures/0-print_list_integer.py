e3d62e005126f8351bb253c24dc07c45f80cbe91
“zawayaalex” <zawayaalex@gmail.com>
2024-03-16 09:52:19 -0700

    lists

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,4 +2,4 @@
 def print_list_integer(my_list=[]):
     n = len(my_list)
     for i in range(n):
-        print("{}".format(my_list[i]))
+        print("{:d}".format(my_list[i]))
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    n = len(my_list)
-    for i in range (n):
-        print("{}".format(my_list[n - 1]))
-        n -= 1
+    if my_list:
+        my_list.reverse()
+        for i in range(len(my_list)):
+            print("{:d}".format(my_list[i]))
