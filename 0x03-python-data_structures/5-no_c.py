106d6a15e7108ab5b5b0f268ba8c8f513f5b47d1
“zawayaalex” <zawayaalex@gmail.com>
2024-03-16 15:44:02 -0700

    lists

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new = my_string.translate({ord('c'):None})
-    final = new.translate({ord('C'):None})
+    new = my_string.translate({ord('c'): None})
+    final = new.translate({ord('C'): None})
     return final
