84ee19a13d34accde30df9d54b559d143e400025
“zawayaalex” <zawayaalex@gmail.com>
2024-03-18 17:13:45 -0700

    tuple

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    m = 0
+    if len(my_list) == 0:
+        return None
+    else:
+        m = my_list[0]
+        for i in range(0, len(my_list)-1, 1):
+            if m < my_list[i]:
+                m = my_list[i]
+        return m
