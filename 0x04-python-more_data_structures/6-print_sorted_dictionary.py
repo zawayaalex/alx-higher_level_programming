42a93b7e1abaa8f7e6039feac1cc79d86d9b49b6
“zawayaalex” <zawayaalex@gmail.com>
2024-05-30 22:05:40 +0300

    come back

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    sorted_keys = sorted(a_dictionary)
-    for key in sorted_keys:
+    for key in sorted(a_dictionary):
         print(f"{key}: {a_dictionary[key]}")
